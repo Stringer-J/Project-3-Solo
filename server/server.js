c2368e38dfa6176e6f62d84a61c8db63e8ad5351
Josh Stringer <joshstringer@live.com>
2024-09-13 09:18:11 -0500

    server runs with mongodb additions intact

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+require('./config/connection');
+
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
-})
+});
+
