5ea2eae908e41d1e1f42918a8b0033ccc83c8f84
Josh Stringer <joshstringer@live.com>
2024-09-16 13:50:07 -0500

    added starter apollo server/graphql code

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+
 const app = express();
 const PORT = 3001;
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
 
 require('./config/connection');
 
