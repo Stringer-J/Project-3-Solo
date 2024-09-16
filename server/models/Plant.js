584882327c4532e2517ffb0e0a0fc9d00785dd1f
Josh Stringer <joshstringer@live.com>
2024-09-16 11:05:07 -0500

    basic plant model finished

--- server/models/Plant.js
+++ server/models/Plant.js
@@ -4,24 +4,31 @@ const { Schema } = mongoose;
 
 const plantSchema = new Schema({
     state: {
-
+        type: Schema.Types.ObjectId,
+        ref: 'State',
+        required: true
     },
     name: {
-
+        type: String,
+        required: true
     },
     startup_id: {
-
+        type: Schema.Types.ObjectId,
+        ref: 'StartUp'
     },
     bug_id: [
         {
-
+            type: Schema.Types.ObjectId,
+            ref: 'Bug'
         }
     ],
     helpful_id: {
-
+        type: Schema.Types.ObjectId,
+        ref: 'Helpful'
     },
     maintenance_id: {
-        
+        type: Schema.Types.ObjectId,
+        ref: 'Maintenance'
     }
 });
 
