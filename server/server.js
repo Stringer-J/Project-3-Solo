fe12757be0c2ebb30ff28c6dccf68e6bacf6ed32
Josh Stringer <joshstringer@live.com>
2024-09-16 14:25:07 -0500

    added more apollo server code to server.js and filled out index.js in schema

--- server/server.js
+++ server/server.js
@@ -2,16 +2,41 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 
+const { typeDefs, resolvers } = require('./schema');
+require('./config/connection');
+
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 });
 
-require('./config/connection');
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
+const startApolloServer = async () => {
+    await server.start();
+
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+
+    app.use('/graphql', expressMiddleware(server, {
+        context: authMiddleware
+    }));
+
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/dist')));
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+        });
+    }
+
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`);
+        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+};
+
+startApolloServer();
+
 
