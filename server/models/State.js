9784c34ea1a963e164a8d932db9eafd333b38e99
Josh Stringer <joshstringer@live.com>
2024-09-16 11:01:07 -0500

    basic state model finished

--- server/models/State.js
+++ server/models/State.js
@@ -4,22 +4,22 @@ const { Schema } = mongoose;
 
 const stateSchema = new Schema({
     heat: {
-
+        type: String
     },
     shade: {
-
+        type: String
     },
     water: {
-
+        type: String
     },
     plants: [
         {
-
+            type: String
         }
     ],
     bugs: [
         {
-
+            type: String
         }
     ]
 });
