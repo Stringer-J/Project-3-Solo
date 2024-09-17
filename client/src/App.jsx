4ae74c2506c89070950cba60eb073ae83aa272d2
Josh Stringer <joshstringer@live.com>
2024-09-17 15:10:13 -0500

    added test buttons to route to each page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   return (
     <>
       <h1>Josh Is A Person</h1>
+      <button>Home</button><br></br><br></br>
+      <button>Sign Up</button><br></br><br></br>
+      <button>Login</button><br></br><br></br>
+      <button>Profile</button><br></br><br></br>
+      <button>Area Info</button><br></br><br></br>
+      <button>Plant Info</button>
     </>
   )
 }
