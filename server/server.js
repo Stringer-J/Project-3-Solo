20338c810da5f7e18e58f6fd8fa8f1ad78757f57
Josh Stringer <joshstringer@live.com>
2024-09-17 11:39:48 -0500

    fixed typeDefs and resolvers to get graphql to work

--- server/server.js
+++ server/server.js
@@ -8,32 +8,38 @@ require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 });
 
-
 const startApolloServer = async () => {
-    await server.start();
+    console.log('HELLO THIS IS startApolloServer!!!!!!!');
+    try {
+        await server.start();
 
-    app.use(express.urlencoded({ extended: false }));
-    app.use(express.json());
-
-    app.use('/graphql', expressMiddleware(server));
-
-    if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/dist')));
-
-        app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+        app.use(express.urlencoded({ extended: false }));
+        app.use(express.json());
+    
+        app.use('/graphql', expressMiddleware(server));
+    
+        if (process.env.NODE_ENV === 'production') {
+            app.use(express.static(path.join(__dirname, '../client/dist')));
+    
+            app.get('*', (req, res) => {
+                res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+            });
+        }
+    
+        app.listen(PORT, () => {
+            console.log(`Server running at http://localhost:${PORT}`);
+            console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
         });
+    } catch (error) {
+        console.error('Error starting Apollo Server:', error);
     }
 
-    app.listen(PORT, () => {
-        console.log(`Server running at http://localhost:${PORT}`);
-        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-    });
 };
 
 startApolloServer();
