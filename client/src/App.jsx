7564fccb46ad4bb50e03fa03f717ced2914ed0c7
Josh Stringer <joshstringer@live.com>
2024-09-17 15:51:45 -0500

    set up routes for react and they all work, which is super cool

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,48 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Signup from './pages/Signup/Signup';
+import Login from './pages/Login/Login';
+import Profile from './pages/Profile/Profile';
+import AreaInfo from './pages/AreaInfo/AreaInfo';
+import PlantInfo from './pages/PlantInfo/PlantInfo';
 
 function App() {
   return (
     <>
+    <Router>
       <h1>Josh Is A Person</h1>
-      <button>Home</button><br></br><br></br>
-      <button>Sign Up</button><br></br><br></br>
-      <button>Login</button><br></br><br></br>
-      <button>Profile</button><br></br><br></br>
-      <button>Area Info</button><br></br><br></br>
-      <button>Plant Info</button>
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+        <Link to="/signup">
+          <button>Sign Up</button>
+        </Link>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/profile">
+          <button>Profile</button>
+        </Link>
+        <Link to="/area-info">
+          <button>Area Info</button>
+        </Link>
+        <Link to="/plant-info">
+          <button>Plant Info</button>
+        </Link>
+
+        <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/area-info" element={<AreaInfo />} />
+      <Route path="/plant-info" element={<PlantInfo />} />
+    </Routes>
+    </Router>
     </>
   )
 }
 
-export default App
+export default App;
