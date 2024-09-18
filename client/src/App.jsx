eef7544cd52bdf9611ebfc26adb02f360696ca87
Josh Stringer <joshstringer@live.com>
2024-09-18 08:55:31 -0500

    added search page and appropriate references in other files, as well as some css changes to the main nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home';
 import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
 import Profile from './pages/Profile/Profile';
+import Search from './pages/Search/Search';
 import AreaInfo from './pages/AreaInfo/AreaInfo';
 import PlantInfo from './pages/PlantInfo/PlantInfo';
 
@@ -35,15 +36,11 @@ function App() {
             <button>Profile</button>
           </Link>
 
-          <Link to="/area-info">
-            <button>Area Info</button>
-          </Link>
-
-          <Link to="/plant-info">
-            <button>Plant Info</button>
+          <Link to="/search">
+            <button>Search</button>
           </Link>
         </div>
-        
+
       </div>
 
         <Routes>
@@ -54,6 +51,8 @@ function App() {
           <Route path="/login" element={<Login />} />
 
           <Route path="/profile" element={<Profile />} />
+
+          <Route path="/search" element={<Search />} />
 
           <Route path="/area-info" element={<AreaInfo />} />
 
