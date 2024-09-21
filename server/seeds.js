59cacb3a542333b37dd5cf61c53fbc57f19aeb6e
Josh Stringer <joshstringer@live.com>
2024-09-21 13:53:56 -0500

    updated seed data to match new user model

--- server/seeds.js
+++ server/seeds.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 
 const users = [
-    { firstName: 'Josh', lastName: 'Stringer', email: 'joshstringer@live.com', password: '123' },
-    { firstName: 'Mariah', lastName: 'Young', email: 'mariah@yahoo.com', password: '456' },
-    { firstName: 'Alice', lastName: 'Young', email: 'alicehatesemail@google.com', password: '789' }
+    { username: 'Josh_Stringer', email: 'joshstringer@live.com', password: '123' },
+    { username: 'Mariah_Young', email: 'mariah@yahoo.com', password: '456' },
+    { username: 'Alice_Young', email: 'alicehatesemail@google.com', password: '789' }
 ];
 
 const MONGO_URI = 'mongodb://localhost:27017/plant_db';
