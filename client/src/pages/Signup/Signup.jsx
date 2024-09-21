58bb901c9a09057f00ce3f4cde992aa30dad08a7
Josh Stringer <joshstringer@live.com>
2024-09-21 12:37:11 -0500

    redid whole sign up page because i did it without react in mind originally

--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -1,21 +1,78 @@
+import { useState } from 'react';
 import './Signup.css';
 
 const Signup = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        passwordRe: ''
+    });
+
+    //tested state with a console log, turns out it updates with every single keystroke
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((oldData) => {
+            const updatedData = {
+                ...oldData,
+                [name]: value
+            };
+            // console.log('Updated State:', updatedData);
+            return updatedData;
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Signup Form Data:', formData);
+    };
+
     return (
-        <>
         <div className='signupBody'>
-            <h1>Sign Up</h1>
-            <form>
-                <label htmlFor='userName'>User Name: </label>
-                <input type='text' id='userName' name='userName' required placeholder='Enter your Username'></input><br></br>
-                <label htmlFor='email'>Email: </label>
-                <input type='text' id='email' name='email' required placeholder='Enter your Email'></input><br></br>
-                <label htmlFor='password'>Enter your Password: </label>
-                <input type='password' id='password' name='password' required placeholder='Enter your Password'></input><br></br>
-                <button>Submit</button>
-            </form>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor='username'>Username:</label>
+            <input
+                type='text'
+                id='username'
+                name='username'
+                value={formData.username}
+                onChange={handleChange}
+                required placeholder='Enter your Username'
+            /><br /><br />
+
+            <label htmlFor='email'>Email:</label>
+            <input
+                type='text'
+                id='email'
+                name='email'
+                value={formData.email}
+                onChange={handleChange}
+                required placeholder='Enter your Email'
+            /><br /><br />
+
+            <label htmlFor='password'>Password:</label>
+            <input
+                type='password'
+                id='password'
+                name='password'
+                value={formData.password}
+                onChange={handleChange}
+                required placeholder='Enter your Password'
+            /><br /><br />
+
+            <label htmlFor='passwordRe'>Re-enter Password:</label>
+            <input
+                type='password'
+                id='passwordRe'
+                name='passwordRe'
+                value={formData.passwordRe}
+                onChange={handleChange}
+                required placeholder='Re-enter your Password'
+            /><br /><br />
+
+            <button type='submit'>Sign Up</button>
+        </form>
         </div>
-        </>
     )
 }
 
