21d35cd1374bd2813d8932c92dfc2710091cbe0d
Josh Stringer <joshstringer@live.com>
2024-09-21 12:08:45 -0500

    basic sign up form created

--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -3,8 +3,17 @@ import './Signup.css';
 const Signup = () => {
     return (
         <>
-        <div className="signupBody">
+        <div className='signupBody'>
             <h1>Sign Up</h1>
+            <form>
+                <label htmlFor='userName'>User Name: </label>
+                <input type='text' id='userName' name='userName' required placeholder='Enter your Username'></input><br></br>
+                <label htmlFor='email'>Email: </label>
+                <input type='text' id='email' name='email' required placeholder='Enter your Email'></input><br></br>
+                <label htmlFor='password'>Enter your Password: </label>
+                <input type='password' id='password' name='password' required placeholder='Enter your Password'></input><br></br>
+                <button>Submit</button>
+            </form>
         </div>
         </>
     )
