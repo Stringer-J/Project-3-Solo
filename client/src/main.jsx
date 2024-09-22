f6318f3a0fa4211c5d58a51cd40c82b13f09d60a
Josh Stringer <joshstringer@live.com>
2024-09-22 13:00:57 -0500

    added AuthProvider to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { AuthProvider } from './utils/AuthContext.js';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
+  <AuthProvider>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </AuthProvider>
 )
