a6246b1d77612ac7aa4fc81b6cbe6a7349dd2ca0
Josh Stringer <joshstringer@live.com>
2024-09-23 13:52:40 -0500

    updated uri yet again in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoURI = process.env.NODE_ENV === 'production'
-    ? 'mongodb+srv://joshstringer:vHwMvfyHljdttfgC@plantcluster.jfen1.mongodb.net/?retryWrites=true&w=majority&appName=PlantCluster'
+    ? 'mongodb+srv://joshstringer:vHwMvfyHljdttfgC@plantcluster.jfen1.mongodb.net/plant_db?retryWrites=true&w=majority&appName=PlantCluster'
     : 'mongodb://localhost:27017/plant_db';
 
 mongoose.connect(mongoURI, {
