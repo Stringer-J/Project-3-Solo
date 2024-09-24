58ef345bbabbc221fccc2a1c4079f55b0868c70f
Josh Stringer <joshstringer@live.com>
2024-09-24 17:39:50 -0500

    had to fix the mutation for addPlant but now it works everywhere and Im very happy about it

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,14 @@ export const ADD_USER_MUTATION = gql`
 export const ADD_USER_PLANT_MUTATION = gql`
     mutation AddPlant($email: String!, $commonName: String!) {
         addPlant(email: $email, commonName: $commonName) {
-            commonName
+             _id
+            username
+            email
+            password
+            plant {
+                _id
+                commonName
+            }
         }
     }
 `;
