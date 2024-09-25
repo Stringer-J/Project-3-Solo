b767d83091d111717495ba0cc7fb5fcf816badf1
Josh Stringer <joshstringer@live.com>
2024-09-25 11:47:57 -0500

    message now shows up on modal to let you know if the plant was added to your profile or not

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,8 +11,8 @@ export const ADD_USER_MUTATION = gql`
 `;
 
 export const ADD_USER_PLANT_MUTATION = gql`
-    mutation AddPlant($email: String!, $commonName: String!) {
-        addPlant(email: $email, commonName: $commonName) {
+    mutation AddPlant($email: String!, $commonName: String!, $thumbNail: String) {
+        addPlant(email: $email, commonName: $commonName, thumbNail: $thumbNail) {
              _id
             username
             email
